6b8f691e37cecc395c0fe7542cd8fabd4bcb1188
Jen Bird <jenniredfield@hotmail.com>
2018-01-26 10:45:17 +0000

    Changed PORT settings

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require('./app')
-const PORT = process.env.NODE_ENV || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, function(){
     console.log(`Server listening on port ${PORT}....`);
