a43cc8859e0e612b6a0016f6f82a9f16c821c435
Jen Bird <jenniredfield@hotmail.com>
2018-01-29 11:20:44 +0000

    Deleted all functions

--- app.js
+++ app.js
@@ -30,72 +30,7 @@ app.route('/').get((req,res,next)=> {
     res.send('Hello, connected to Do-more server!')
 })
 
-app.route('/auth/twitter/reverse')
-  .post(function(req, res) {
-    console.log('reverse endpoint')
-    request.post({
-      url: 'https://api.twitter.com/oauth/request_token',
-      oauth: {
-        oauth_callback: "http%3A%2F%2Flocalhost%3A3000%2Ftwitter-callback",
-        consumer_key: config.CONSUMER_KEY,
-        consumer_secret: config.CONSUMER_SECRET
-      }
-    }, function (err, r, body) {
-      if (err) {
-        return res.send(500, { message: e.message });
-      }
-      console.log('callback function')
-      var jsonStr = '{ "' + body.replace(/&/g, '", "').replace(/=/g, '": "') + '"}';
-      
-      res.send(JSON.parse(jsonStr));
-    });
-  });
 
-  app.route('/auth/twitter')
-  .get((req, res, next) => {
-   
-    request.post({
-      url: `https://api.twitter.com/oauth/access_token?oauth_verifier`,
-      oauth: {
-        consumer_key: config.CONSUMER_KEY,
-        consumer_secret: config.CONSUMER_SECRET,
-        token: req.query.oauth_token
-      },
-      form: { oauth_verifier: req.query.oauth_verifier }
-    }, function (err, r, body) {
-      if (err) {
-        return res.send(500, { message: err.message });
-      }
-    
-   
-      const bodyString = '{ "' + body.replace(/&/g, '", "').replace(/=/g, '": "') + '"}';
-  
-      const parsedBody = JSON.parse(bodyString);
-      console.log(bodyString)
-      console.log('verifier', req.query.oauth_verifier)
-      console.log("****************************************************************************")
-      req.body['oauth_token'] = parsedBody.oauth_token;
-      req.body['oauth_token_secret'] = parsedBody.oauth_token_secret;
-      req.body['user_id'] = parsedBody.user_id;
-    
-      
-      next();
-    });
-  }, function(a,b,c){
-   
-    console.log("****************************************************************************")
-    console.log(a.query)
-    console.log("****************************************************************************")
-    console.log(a.url)
-    console.log("****************************************************************************")
-    console.log(Object.keys(a))
-      request.post(`https://api.twitter.com/oauth/access_token?oauth_token=${a.query.oauth_token}&oauth_verifier=${a.query.oauth_verifier}`, function(err, r, body){
-      console.log(err)
-      console.log(body)
-  
-      })
-  }
-);
 
 
 module.exports = app;
