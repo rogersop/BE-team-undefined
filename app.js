a86574d667edf7d604e95282cb77791aedd512f6
Jen Bird <jenniredfield@hotmail.com>
2018-01-29 11:41:01 +0000

    creted twit request

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const request = require('request');
+const Twit = require('twit');
 
 const config = {
 
@@ -25,7 +26,26 @@ var corsOption = {
   app.use(bodyParser.json());
 
 
-app.route('/').get((req,res,next)=> {
+app.route('/').post((req,res,next)=> {
+
+    let token = req.body.token;
+    let secret = req.body.secret;
+
+    const T = new Twit({
+        consumer_key:         "al5LN8w9gWcirowNLtxPHtoyf",
+        consumer_secret:       "rj0C0w5tiLFsvTx1ZeKxIrAd7LFR2Q5NN8nz1gul6YbzwV4ZJp",
+        access_token:         token,
+        access_token_secret:  secret,
+        timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests. 
+   
+
+    })
+
+    T.get('/statuses/user_timeline', function(err, data, response){
+      console.log('data', data);
+      console.log('response', reponse)
+      res.send(data);
+    })
 
     res.send('Hello, connected to Do-more server!')
 })
